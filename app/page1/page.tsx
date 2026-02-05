9442b73f1bc84f80ecd208e4a93dd78625b14ee8
Aditya <kosurusai646@gmail.com>
2026-02-05 11:55:59 +0530

    grok

--- app/page1/page.tsx
+++ app/page1/page.tsx
@@ -11,7 +11,7 @@ type ChatMessage = {
     content: string;
     };
 
-    const API_URL = "http://localhost:8000/ask";
+    const API_URL = "https://autoplastic-mario-peakless.ngrok-free.dev";
 
     export default function Page1() {
     const searchParams = useSearchParams();
